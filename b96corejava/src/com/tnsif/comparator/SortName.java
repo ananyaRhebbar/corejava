1c7d0ee8f6c6b86b1c7a05232053b522e2d1b579
Ananya <Ananya@LAPTOP-89M2EG1O>
2024-07-22 17:40:39 +0530

    fourth commit

--- b96corejava/src/com/tnsif/comparator/SortName.java
+++ b96corejava/src/com/tnsif/comparator/SortName.java
@@ -4,4 +4,11 @@ import java.util.Comparator;
 
 public class SortName implements Comparator<Students>{
 
+
+	@Override
+	public int compare(Students o1, Students o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- b96corejava/src/com/tnsif/comparator/SortRoll.java
+++ b96corejava/src/com/tnsif/comparator/SortRoll.java
@@ -1,5 +1,20 @@
 package com.tnsif.comparator;
 
-public class SortRoll implements comparator <Student>{
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortRoll {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Student>ar= new ArrayList<Student>  ()
+				ar.add(new Student(222,"aaa","london"));
+		        ar.add(new Student(242,"bbb","usa"));
+		    	ar.add(new Student(232,"ccc","paris"));
+		    	
+		    	System.out.println("-----------");
+		    	Collections.sort(ar,new SortRoll());
+		         
+	}
 
 }
